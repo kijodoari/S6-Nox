3ae10e871c9b3c4eab7ba972ec087f3f295dc5cf
arter97 <qkrwngud825@gmail.com>
2017-01-24 17:24:49 +0100

    sched: convert hmp tunables into static
    These aren't used externally.
    
    Maybe : TODO - make use of __read_mostly.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -1584,11 +1584,11 @@ static inline void __update_group_entity_contrib(struct sched_entity *se) {}
  * tweaking suit particular needs.
  */
 
-unsigned int hmp_up_threshold = 479;
-unsigned int hmp_down_threshold = 214;
+static unsigned int hmp_up_threshold = 479;
+static unsigned int hmp_down_threshold = 214;
 
-unsigned int hmp_semiboost_up_threshold = 400;
-unsigned int hmp_semiboost_down_threshold = 150;
+static unsigned int hmp_semiboost_up_threshold = 479;
+static unsigned int hmp_semiboost_down_threshold = 150;
 
 /* Global switch between power-aware migrations and classical GTS. */
 static unsigned int hmp_power_migration = 0;
@@ -3873,27 +3873,27 @@ static DEFINE_RAW_SPINLOCK(hmp_sysfs_lock);
 #define YIELD_CORRECTION_TIME 10000000 /* nanoseconds */
 
 #ifdef CONFIG_SCHED_HMP_PRIO_FILTER
-unsigned int hmp_up_prio = NICE_TO_PRIO(CONFIG_SCHED_HMP_PRIO_FILTER_VAL);
+static unsigned int hmp_up_prio = NICE_TO_PRIO(CONFIG_SCHED_HMP_PRIO_FILTER_VAL);
 #endif
-unsigned int hmp_next_up_threshold = 4096;
-unsigned int hmp_next_down_threshold = 4096;
+static unsigned int hmp_next_up_threshold = 4096;
+static unsigned int hmp_next_down_threshold = 4096;
 
 #ifdef CONFIG_SCHED_HMP_LITTLE_PACKING
-unsigned int hmp_packing_enabled = 1;
-unsigned int hmp_full_threshold = (NICE_0_LOAD * 9) / 8;
+static unsigned int hmp_packing_enabled = 0;
+static unsigned int hmp_full_threshold = (NICE_0_LOAD * 9) / 8;
 #endif
 
-static inline int hmp_boost(void)
+static inline bool hmp_boost(void)
 {
 	u64 now = ktime_to_us(ktime_get());
-	int ret;
+	bool ret;
 	unsigned long flags;
 
 	raw_spin_lock_irqsave(&hmp_boost_lock, flags);
 	if (hmp_boost_val || now < hmp_boostpulse_endtime)
-		ret = 1;
+		ret = true;
 	else
-		ret = 0;
+		ret = false;
 	raw_spin_unlock_irqrestore(&hmp_boost_lock, flags);
 
 	return ret;
@@ -4094,7 +4094,7 @@ static u64 hmp_variable_scale_convert(u64 delta)
 	u64 high = delta >> 32ULL;
 	u64 low = delta & 0xffffffffULL;
 
-	if (hmp_semiboost()) {
+	if (hmp_semiboost_val) {
 		low *= hmp_data.semiboost_multiplier;
 		high *= hmp_data.semiboost_multiplier;
 	} else {
@@ -4412,7 +4412,7 @@ int set_hmp_aggressive_yield(int enable)
 
 int get_hmp_boost(void)
 {
-	return hmp_boost();
+	return hmp_boost() ? 1 : 0;
 }
 
 int get_hmp_semiboost(void)
